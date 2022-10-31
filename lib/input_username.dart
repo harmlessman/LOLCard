9fa7169b10e04da7f5c9272457dd755f3b70cfe9
junyongleeme <leejunyong1150@gmail.com>
2022-10-31 16:55:40 +0900

    Add event when username is entered(LoadingDialog, FailDialog)

--- lib/input_username.dart
+++ lib/input_username.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'data_crawling.dart';
+import 'package:lolcard/more_info/select_lane.dart';
 
 class InputUsername extends StatefulWidget {
   const InputUsername({Key? key}) : super(key: key);
@@ -9,7 +11,88 @@ class InputUsername extends StatefulWidget {
 
 class _InputUsernameState extends State<InputUsername> {
   TextEditingController inputController = TextEditingController();
-  String? userName;
+  String userName = '';
+  bool loading = false;
+
+  LoadingDialog(BuildContext context){
+    AlertDialog alert=AlertDialog(
+      content: Row(
+        children: [
+          CircularProgressIndicator(),
+          Container(margin: EdgeInsets.only(left: 5),child:Text("Loading" )),
+        ],),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)),
+    );
+    showDialog(barrierDismissible: false,
+      context:context,
+      builder:(BuildContext context){
+        return alert;
+      },
+    );
+  }
+
+  FailDialog(BuildContext context, dynamic code){
+    String text = 'Null';
+    switch (code){
+      case 0:
+        text = 'Except Error (normally network issue)';
+        break;
+      case 1:
+        text = 'op.gg Server Error';
+        break;
+      case 2:
+        text = 'An Unregistered Summoner';
+        break;
+    }
+    showDialog(barrierDismissible: false,
+      context:context,
+      builder:(BuildContext context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)),
+          title: Column(
+            children: [Text("Failure")],
+          ),
+          content: Row(
+            children: [Text(text)],
+          ),
+          actions: [
+            TextButton(
+              onPressed: ()=>{
+                Navigator.pop(context)
+              },
+              child: Text("OK"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+
+
+  EnterEvent() async{
+    userName = inputController.text;
+    LoadingDialog(context);
+    // await 붙이면 CardData, 안붙이면 Future<dynamic>
+    dynamic data = await dataCrawling(userName);
+
+    Navigator.pop(context);
+
+    // if error occur
+    if (int.tryParse(data.toString()) != null){
+      FailDialog(context, data);
+      return int.tryParse(data.toString());
+    }
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> SelectLane())
+    );
+    return data;
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +119,6 @@ class _InputUsernameState extends State<InputUsername> {
                     padding: EdgeInsets.all(20.0),
                     child:TextField(
                       controller: inputController,
-
                       decoration: InputDecoration(
                         focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(10.0)),
@@ -49,15 +131,12 @@ class _InputUsernameState extends State<InputUsername> {
 
                         labelText: 'username',
                         hintText: 'Enter your username',
-
-
                       ),
                     )
 
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-
                   children: <Widget>[
                     Container(
                       padding: EdgeInsets.all(20.0),
@@ -74,22 +153,17 @@ class _InputUsernameState extends State<InputUsername> {
                     Container(
                       padding: EdgeInsets.all(20.0),
                       child: ElevatedButton(
-                        onPressed: ()=>{
-                          userName = inputController.text,
-                          print(userName),
-
-                        },
+                        onPressed: EnterEvent,
                         child: Container(
                           child: Text('Enter'),
                         ),
                       ),
-                    )
+                    ),
+
                   ],
                 )
-
               ],
             )
-
         )
     );
   }
---
+++ lib/more_info/select_lane.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SelectLane extends StatefulWidget {
+  const SelectLane({Key? key}) : super(key: key);
+
+  @override
+  State<SelectLane> createState() => _SelectLaneState();
+}
+
+class _SelectLaneState extends State<SelectLane> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
