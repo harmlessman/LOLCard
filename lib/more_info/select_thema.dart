3ca86a2b7f6ec80b3d43f49920dc61a824eead79
junyongleeme <leejunyong1150@gmail.com>
2022-11-01 23:55:00 +0900

    add select_lane, select_thema

---
+++ lib/more_info/select_thema.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectThema extends StatefulWidget {
+  const SelectThema({Key? key}) : super(key: key);
+
+  @override
+  State<SelectThema> createState() => _SelectThemaState();
+}
+
+class _SelectThemaState extends State<SelectThema> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
